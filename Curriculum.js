1b1b7186295e1581f16c5b9c270c635c05780df1
J. Andrade <ndrd@ciencias.unam.mx>
2016-01-26 22:30:16 -0600

    Curricuulum

---
+++ Curriculum.js
@@ -0,0 +1,89 @@
+Curriculum = {};
+
+Curriculum.datosPersonales = {
+	nombre : 'Jonathan de Jesús Andrade López',
+	celular : '55 22 83 78 04',
+	emails : ['ndrd@ciencias.unam.mx', 'ndrd.thn@gmail.com'],
+	estadoCivil : 'soltero',
+	fechaDeNacimiento : '1994-05-04',
+	domicilio : {
+		colonia : 'Condesa',
+		calle : 'Antonio Sola',
+		nExterior : 33,
+		nInterior : 4,
+		codigoPostal : 06140
+	},
+	disponibilidadDeHorario : '2pm-8pm'
+};
+
+Curriculum.educacion : {
+	universidad : {
+		nombre : 'Universidad Nacional Autónoma de México',
+		facultad : 'Facultad de ciencias',
+		carrera : 'Ciencias de la Computacion'
+		anioInicio : 2013
+		anioGraducion : 2017
+		semestreActual : 6
+	}
+};
+
+Curriculum.habilidades : {
+	lenguajesDeProgramacion : [
+		"Java", "Javascript",
+		"Vala", "Ruby",
+		"PHP","Python",
+		"C","C++",
+		"C#","Haskell"
+	],
+	frameworks : [
+		"Ruby on Rails",
+		"App Engine",
+		"Express & NodeJS",
+		"Apache Cordova",
+		"Bootstrap",
+		"Polymer",
+		"Flask",
+		"Composer"
+	],
+	basesDeDatos : [
+		'Postgres',
+		'MySql',
+		'redis',
+		'mongo'
+	],
+	sysadmin : [
+		'haproxy',
+		'docker',
+		'nginx',
+		'apache httpd',
+	],
+	extras : [
+		'habilidades en CSS',
+		'experiencia en Git'
+		'experiencia en vision por computadora',
+		'jugar con redes neuronales en mis ratos libres',
+	]
+}
+
+Curriculum.extras : {
+	valores : [
+		"trabajo en equipo",
+		"iniciativa",
+		"responsabilidad",
+		"liderazgo",
+		"perseverancia",
+		"determinación",
+		"resiliencia"
+	],
+	hobbies : [
+		'correr',
+		'aprender idiomas en internet',
+		'acampar'
+	]
+	areasDeInteres : [
+		'filosofia',
+		'inteligencia aritificial',
+		'sistemas auto-organizantes',
+		'sociedades artificiales'
+	]
+}
